ba13409e1f02591cff7dd7d08d707c8eaeb7910e
Akbar Ridho Arrafi <akbarridhoarrafi@gmail.com>
2026-06-18 21:58:34 +0700

    Update: Rename 'proyek_akhir' to 'bioskop'

--- src/bioskop/Ambacinema.java
+++ src/bioskop/Ambacinema.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyek_akhir;
+package bioskop;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
